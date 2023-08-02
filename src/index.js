11e869b9680cab7f164f6901c448bad3fbed8179
Hemant Konda <hemanthkon0591@gmail.com>
2023-08-02 20:59:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -15,4 +14,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
